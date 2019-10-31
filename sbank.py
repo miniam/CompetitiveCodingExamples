9ec5230e6243aa49880b0a88c313e3ae22747121
Abhinav Dhere <abhinavdhere@gmail.com>
2019-10-31 21:39:05 +0530

    Added comments

--- sbank.py
+++ sbank.py
@@ -1,10 +1,13 @@
+## Solution to sbank from spoj
+# Problem statement can be found at https://www.spoj.com/problems/SBANK/
+
 t = int(input())
 
 for iterNum in range(t):
 	n = int(input())
 	accDict = {} 
 	for acc in range(n):
-		accNum = input()
+		accNum = input()				# account number is read and added to dictionary
 		if accNum in accDict.keys():
 			accDict[accNum] += 1
 		else:
@@ -12,5 +15,5 @@ for iterNum in range(t):
 	for num in sorted(accDict.keys()):
 		repeatVal = accDict[num]
 		print(num+' '+str(repeatVal))
-	print()
+	print()								# for blank lines
 	input()
